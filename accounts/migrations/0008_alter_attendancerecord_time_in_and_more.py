28fb9b42348b6d94a533f3ec8d2bfc84a0a56888
Mark Justine Cruz <mrkjstncrz15@gmail.com>
2026-02-14 12:34:45 +0800

    yaya

---
+++ accounts/migrations/0008_alter_attendancerecord_time_in_and_more.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0007_rename_monthly_amount_salarygrade'),
+    ]
+
+    operations = []
---
+++ accounts/migrations/0009_alter_attendancerecord_time_in_and_more.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0008_alter_attendancerecord_time_in_and_more'),
+    ]
+
+    operations = []
