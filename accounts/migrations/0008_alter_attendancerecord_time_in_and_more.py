e9912b67d41edc355bab66ae412dcdc1fcad3a7d
Mark Justine Cruz <mrkjstncrz15@gmail.com>
2026-02-14 12:41:30 +0800

    yayads

--- accounts/migrations/0008_alter_attendancerecord_time_in_and_more.py
+++ accounts/migrations/0008_alter_attendancerecord_time_in_and_more.py
@@ -3,7 +3,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0007_rename_monthly_amount_salarygrade'),
+        ('accounts', '0006_salarygrade'),
     ]
+
 
     operations = []
